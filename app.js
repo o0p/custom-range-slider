0652aa46f57deb736f7dfba1492e4d91f485410f
Russell <russelldev@protonmail.com>
2021-03-05 13:59:13 +0000

    Add min max vars

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ range.addEventListener('input', (e) => {
     const label_width = getComputedStyle(label).getPropertyValue('width');
     const num_width = +range_width.substring(0, range_width.length - 2);
     const num_label_width = +label_width.substring(0, range_width.length - 2);
-
+    const min = +e.target.min;
+    const max = +e.target.max;
     label.innerHTML = value;
 });
