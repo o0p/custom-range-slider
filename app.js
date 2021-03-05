fb6c3cdc3552503ce361bb2d94821c7995d0286d
Russell <russelldev@protonmail.com>
2021-03-05 13:32:43 +0000

    Link range label with slider position

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 const range = document.getElementById('range');
+range.addEventListener('input', (e) => {
+    const value = +e.target.value;
+    const label = e.target.nextElementSibling;
+    label.innerHTML = value;
+});
