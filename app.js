ee08d8799b80172cb15418fb9c008f6bb70a36f6
Russell <russelldev@protonmail.com>
2021-03-05 13:58:20 +0000

    Add range and label width and convert them to numbers

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const range = document.getElementById('range');
 range.addEventListener('input', (e) => {
     const value = +e.target.value;
     const label = e.target.nextElementSibling;
+    const range_width = getComputedStyle(e.target).getPropertyValue('width');
+    const label_width = getComputedStyle(label).getPropertyValue('width');
+    const num_width = +range_width.substring(0, range_width.length - 2);
+    const num_label_width = +label_width.substring(0, range_width.length - 2);
+
     label.innerHTML = value;
 });
