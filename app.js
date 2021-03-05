8abe131de65c96cbcda7e607e127d4ece2f6488a
Russell <russelldev@protonmail.com>
2021-03-05 14:04:47 +0000

    Add label shifter and fix num_label_with bug

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ range.addEventListener('input', (e) => {
     const range_width = getComputedStyle(e.target).getPropertyValue('width');
     const label_width = getComputedStyle(label).getPropertyValue('width');
     const num_width = +range_width.substring(0, range_width.length - 2);
-    const num_label_width = +label_width.substring(0, range_width.length - 2);
+    const num_label_width = +label_width.substring(0, label_width.length - 2);
     const min = +e.target.min;
     const max = +e.target.max;
     const left = value * (num_width / max) - num_label_width / 2;
+    label.style.left = `${left}px`
 
 
     label.innerHTML = value;
